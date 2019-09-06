e2c5b0f2874f7eb1e3857733adb048e481785f0f
Nuthan Kumar Mallavaram <nmallavaram@salesforce.com>
2019-09-05 20:17:54 -0700

    content component

---
+++ web-front/src/components/Content.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+/**
+ * Main Content
+ * @param {object} props Component props
+ * @visibleName content page
+ */
+export function Content({ props }) {
+    return (
+        <div className="page-content p-5" id="content">
+            <Toggle />
+        </div>
+    );
+}
+
+
+/**
+ * Toggle sidebar
+ * @param {object} props Component props
+ * @visibleName content page
+ */
+export function Toggle({ props }) {
+    const onClickToggle = () => {
+        $('#sidebarCollapse').on('click', function() {
+            $('#sidebar, #content').toggleClass('active');
+          });
+    }
+    return (
+        <button id="sidebarCollapse" onClick = {onClickToggle} type="button" className="btn btn-light bg-white rounded-pill shadow-sm px-4 mb-4">
+            <i className="fa fa-bars mr-2"></i>
+            <small class="text-uppercase font-weight-bold">Toggle</small>
+        </button>
+    );
+}
