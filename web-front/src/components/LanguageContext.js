bd133d87f263458356118932032491f3f92ea27c
Nuthan Kumar Mallavaram <nmallavaram@salesforce.com>
2020-04-17 12:30:15 -0700

    language and theme context

---
+++ web-front/src/components/LanguageContext.js
@@ -0,0 +1,5 @@
+const LanguageContext = React.createContext('en')
+class LanguageProvider extends React.Component {
+  /* code */
+}
+const LanguageConsumer = LanguageContext.Consumer
---
+++ web-front/src/components/ThemeContext.js
@@ -0,0 +1,5 @@
+const ThemeContext = React.createContext('light')
+class ThemeProvider extends React.Component {
+  /* code */
+}
+const ThemeConsumer = ThemeContext.Consumer
